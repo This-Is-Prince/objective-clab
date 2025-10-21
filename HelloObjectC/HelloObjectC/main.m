8a4b8c7e76af586c0a25089b90e6f4feef7b5532
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-10-21 16:50:22 +0530

    feat: Hello program in Objective-C

---
+++ HelloObjectC/HelloObjectC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloObjectC
+//
+//  Created by Prince on 21/10/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return EXIT_SUCCESS;
+}
